fca003b685a24443e8d5d9bfc149e129476098ca
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-09-19 02:31:32 +0500

    extra pages removed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import "./App.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Homepage from "./pages/home";
-import Aboutpage from "./pages/about";
 import Portfolio from "./pages/portfolio";
-import Feedback from "./pages/feedback";
-import Contact from "./pages/contact";
-import NoPage from "./pages/nopage";
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis } from 'lenis/react'
 
 function App() {
   gsap.registerPlugin(ScrollTrigger);
@@ -20,11 +16,8 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route index element={<Homepage />} />
-              <Route path="/about" element={<Aboutpage />} />
               <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/feedback" element={<Feedback />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<NoPage />} />
+              {/*<Route path="*" element={<NoPage />} />*/}
             </Routes>
           </BrowserRouter>
       </ReactLenis>
