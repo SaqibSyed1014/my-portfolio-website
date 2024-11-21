b5ca2f9b4eec16d9b77ed25b9da46bf284819c42
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-11-21 14:11:24 +0500

    footer image updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
       <footer className="relative lg:sticky lg:bottom-0 z-[0] grid grid-cols-12 items-stretch gap-4 px-3 py-8 md:p-6">
           <div className="md:col-span-4">
               <div className="footer-image rounded-2xl overflow-hidden max-md:hidden">
-                  <img src="https://www.juulvrasdonk.nl/_next/image?url=%2Fimages%2Fprofile.webp&w=3840&q=75" alt=""/>
+                  <img src="/images/my_image.png" alt="Saqib Syed Profile Image" className="h-full" />
               </div>
           </div>
           <div className="col-span-12 md:col-span-8">
               <div className="flex flex-col items-stretch gap-3 max-md:mb-4">
                   <div className="flex items-start max-sm:gap-3 sm:items-end max-sm:flex-col">
                       <h2 className="footer-heading">Lets connect</h2>
-                      <BaseButton type="button" label="Get in touch" showDot={true} classes="" />
+                      <BaseButton type="button" label="Get in touch" showDot={true} link="https://www.upwork.com/freelancers/uidevelopment" />
                   </div>
                   <ul className="social-links flex gap-6">
                       <li><a href="https://github.com/saqibsyed1014" target="_blank">GITHUB</a></li>
